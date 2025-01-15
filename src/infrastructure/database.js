c844251a251b51ba05addb8a8833f4f6d9658570
SamuelRBatista <samuka-srb@live.com>
2025-01-15 11:19:19 -0300

    chore:15012025 => Ajuste no env e database.

--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -1,13 +1,15 @@
-// src/infrastructure/database.js
 const mysql = require("mysql2");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "samuka.201232",
-  database: "quiz_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 const promisePool = pool.promise();
 
 module.exports = promisePool;
+
